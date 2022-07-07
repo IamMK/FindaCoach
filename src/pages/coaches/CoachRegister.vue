9ad75980f46440c840c20adc9a395783b139c78c
IamMK <malk@int.pl>
2022-07-07 20:52:33 +0200

    Register form added

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import CoachForm from "@/components/coaches/CoachForm.vue";
+import BaseCard from "@/components/ui/BaseCard.vue";
+</script>
+
 <template>
-  <section>Register</section>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form />
+    </base-card>
+  </section>
 </template>
